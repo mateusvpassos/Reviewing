6b3a92d0e4a6ff7d3279fd141d72fb31de493020
Mateus Vinicius dos Passos <mviniciusdospassos@gmail.com>
2020-12-14 13:40:30 -0300

    Adicionando a base do projeto

---
+++ src/main/java/com/start/reviewing/ReviewingApplication.java
@@ -0,0 +1,13 @@
+package com.start.reviewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewingApplication.class, args);
+    }
+
+}
